5fccbdeda99464b71a90c515c84efd020fdf41d3
ngohoang2110 <hoanggg2110@gmail.com>
2022-09-07 21:33:36 +0700

    Initial Project

---
+++ src/main/java/com/srs/proto/MarketProtoApplication.java
@@ -0,0 +1,13 @@
+package com.srs.proto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketProtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketProtoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/srs/proto/MarketProtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srs.proto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketProtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
